3c635e3f02aa9fcbcca3bc0bccc6614921188fcf
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-24 15:53:43 -0700

    Commiting minor changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 //components
 import Dashboard from './components/admin/Dashboard';
 import Home from './components/Home';
-import Login from './components/Login';
+import LoginForm from './components/Login';
+import PrivateRoute from './components/admin/PrivateRoute'
 
 //context
 import StoryContext from '../src/contexts/StoryContext';
@@ -12,9 +14,8 @@ import StoryContext from '../src/contexts/StoryContext';
 function App() {
   return (
     <div className="App">
-      
-      <Home /> 
-      <Login />
+      <Home />
+      <LoginForm />
     </div>
   );
 }
